4da09d7de07551e41d6f2cb89130a3d984437cb5
Herald2026 <heraldopenaccess.us@gmail.com>
2026-04-28 14:58:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-fetch('journals.json')
+fetch('data.json')
   .then(res => res.json())
   .then(data => {
+
     const container = document.getElementById("journalContainer");
 
-    data.forEach(journal => {
+    // extract unique journals
+    const journals = [...new Set(data.map(item => item.journal))];
+
+    console.log("Total Journals Loaded:", journals.length);
+
+    journals.forEach(journal => {
+
       const card = document.createElement("div");
       card.className = "journal-card";
 
       card.innerHTML = `
-        <h3>${journal.name}</h3>
-        <button onclick="openJournal('${journal.name}')">
+        <h3>${journal}</h3>
+        <button onclick="openJournal('${journal}')">
           🔍 View Articles
         </button>
       `;
 
       container.appendChild(card);
     });
-  });
 
-function openJournal(name) {
-  localStorage.setItem("selectedJournal", name);
-  window.location.href = "articles.html";
+  })
+  .catch(err => console.error("Error loading data:", err));
+
+function openJournal(journal) {
+  localStorage.setItem("selectedJournal", journal);
+  window.location.href = "article.html";
 }
